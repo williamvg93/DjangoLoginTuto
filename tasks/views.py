013c7a100957cd419ae3a6d32ef8790bc0b4a7b2
williamvg93 <sonico_5071@hotmail.com>
2024-09-02 18:14:20 -0500

    feat: :sparkles: The user registration page layout is created

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+# from django.contrib.auth.forms import UserCreationForm
+from tasks.forms import NewUserForm
 
 # Create your views here.
 def mainPage(request):
 
-    return render(request, "layouts/tasks/signup.html", {"pageTitle" : "SignUp", "mainPageTitle" : "SignUp"})
+    form = NewUserForm()
+    #if "id_usable_password" in form.fields:
+     #   del form.fields["id_usable_password"]
+
+    return render(
+        request,
+        "layouts/tasks/signup.html",
+        {"pageTitle": "SignUp", "mainPageTitle": "SignUp", "form": form},
+    )
