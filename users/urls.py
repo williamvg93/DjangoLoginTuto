219f6a136c16f3f1d0e580e1da2a4e2f6bfd9cb3
williamvg93 <sonico_5071@hotmail.com>
2024-09-03 19:48:54 -0500

    feat: :sparkles: Functions and their respective layouts were created for logging in and out

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('', signUp, name='signUp'),
+    path("", signUp, name="signUp"),
+    path("logout/", logout_user, name="logout"),
+    path("signIn/", signIn, name="signIn"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from users.forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def signUp(request):
@@ -18,9 +19,39 @@ def signUp(request):
                 return HttpResponse(f"Error: {e}")
         else:
             return HttpResponse(f"Passwords do not match !!!")
-    form = NewUserForm()
-    return render(
-        request,
-        "layouts/user/signup.html",
-        {"pageTitle": "SignUp", "mainPageTitle": "SignUp", "form": form},
-    )
+    else:
+        form = NewUserForm()
+        return render(
+            request,
+            "layouts/user/signUp.html",
+            {"pageTitle": "SignUp", "mainPageTitle": "SignUp", "form": form},
+        )
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("home")
+
+
+def signIn(request):
+
+    if request.method == 'POST':
+        print(request.POST)
+        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        print(user)
+        if user is None:
+            return render(
+                request,
+                "layouts/user/signin.html",
+                {
+                    "pageTitle": "SignIn",
+                    "mainPageTitle": "SignIn",
+                    "form": AuthenticationForm,
+                    "msgError" : "Invalid username or password",
+                },
+            )
+        else:
+            login(request, user)
+            return redirect('home')
+    else:
+        return render(request, "layouts/user/signin.html", {"pageTitle": "SignIn", "mainPageTitle": "SignIn", "form" : AuthenticationForm})
